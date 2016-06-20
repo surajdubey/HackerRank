2dc892125f98c7fc0dfc593462bac6b84df1fe0f
suraj <suraj.dubey@hotmail.com>
2016-06-20 15:36:22 +0530

    trees data structures, preorder, postorder, inorder

---
+++ datastructures/trees/TreeOperations.java
@@ -0,0 +1,40 @@
+package datastructures.trees;
+
+public class TreeOperations {
+    class Node {
+        int data;
+        Node left;
+        Node right;
+    }
+    
+    void preOrder(Node root) {
+        if(root == null) {
+            return;
+        }
+        
+        System.out.print(root.data + " ");
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+    
+    void postOrder(Node root) {
+        if(root == null) {
+            return;
+        }
+        
+        postOrder(root.left);
+        postOrder(root.right);
+        System.out.print(root.data + " ");
+    }
+    
+    void inOrder(Node root) {
+        if(root == null) {
+            return;
+        }
+        
+        inOrder(root.left);
+        System.out.print(root.data + " ");
+        inOrder(root.right);
+    }
+    
+}
